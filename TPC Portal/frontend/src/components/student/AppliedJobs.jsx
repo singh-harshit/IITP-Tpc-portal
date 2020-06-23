db7c2a2305c0181473eba11bb70781efdc01ba26
CITIZENDOT <appaji12368@gmail.com>
2020-06-23 21:35:50 +0530

    Added Tables

--- TPC Portal/frontend/src/components/student/AppliedJobs.jsx
+++ TPC Portal/frontend/src/components/student/AppliedJobs.jsx
@@ -2,57 +2,96 @@ import React from "react";
 import axios from "axios";
 
 export class StudentAppliedJobs extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-  this.state = {
-    studId: props.match.params.id,
-    rawData: [],
-    jsonDataForTable: [],
-  }}
-  componentDidMount() {
-    this.getStudentInfo();
-    //this.dataToJson();
+    this.state = {
+      studId: props.match.params.id,
+      jobsList: [],
+      jsonDataForTable: [],
+    };
   }
-
-  getStudentInfo = () => {
+  componentDidMount() {
+    /* Fetch Data */
     axios
       .get("/student/applied/jobs/" + this.state.studId)
       .then((response) => {
         const data = response.data;
         this.setState({
-          rawData: data,
+          jobsList: data.studentWithAppliedJobs.appliedJobs,
         });
-        console.log("Student Job List Received!!");
-        console.log(this.state.rawData);
+        console.log("State Variable Set!");
+        /* Make JSON */
+        const jobsList = this.state.jobsList;
+        let jsonData = [];
+        for (let i = 0; i < jobsList.length; i++) {
+          if (jobsList[i].jobId) {
+            let jsonDataObject = {
+              SNo: i + 1,
+              company: jobsList[i].jobId.companyName,
+              title: jobsList[i].jobId.jobTitle,
+              classification: jobsList[i].jobId.jobCategory,
+              schedule: jobsList[i].jobId.schedule.ppt,
+              status: jobsList[i].jobStatus,
+            };
+            jsonData.push(jsonDataObject);
+          }
+        }
+        this.setState({
+          jsonDataForTable: jsonData,
+        });
+        console.log("Json Variable Set!");
         return null;
       })
       .catch((error) =>
         console.log("Error receiving in Student Job List", error)
       );
+  }
+
+  displayHeaders = () => {
+    const headers = [
+      "S.No",
+      "Company Name",
+      "Title",
+      "Classification",
+      "Schedule",
+      "Status",
+    ];
+    return headers.map((header, index) => {
+      return <th key={index}>{header}</th>;
+    });
   };
 
-  dataToJson = () => {
-    const jobsList = this.state.rawData.studentWithAppliedData.appliedJobs;
-    let jsonData = [];
-    for (let i = 0; i < jobsList.length; i++) {
-      let jsonDataObject = {
-        SNo: i + 1,
-        company: jobsList[i].jobId.companyName,
-        title: jobsList[i].jobId.jobTitle,
-        classification: jobsList[i].jobId.jobCategory,
-        schedule: jobsList[i].jobId.schedule.ppt,
-        status: jobsList[i].jobStatus,
-      };
-
-      jsonData.push(jsonDataObject);
-    }
-
-    this.setState({
-      jsonDataForTable: jsonData,
+  displayTable = () => {
+    const jsonData = this.state.jsonDataForTable;
+    return jsonData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <RenderRow key={index} data={row} />
+        </tr>
+      );
     });
   };
 
   render() {
-    return null;
+    // console.log("Raw Data: ", this.state.jobsList);
+    // console.log("Json Data: ", this.state.jsonDataForTable);
+    return (
+      <div className="AppliedJobs">
+        <table className="table">
+          <thead className="thead-light">
+            <tr>{this.displayHeaders()}</tr>
+          </thead>
+
+          <tbody>{this.displayTable()}</tbody>
+        </table>
+      </div>
+    );
   }
 }
+
+const RenderRow = (props) => {
+  const keys = Object.keys(props.data);
+  return keys.map((key, index) => {
+    return <td key={index}>{props.data[key]}</td>;
+  });
+};
